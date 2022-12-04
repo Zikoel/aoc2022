168da19eb29016e39367706a70a77fcfd9109803
Elia Mazzuoli <zikoel@gmail.com>
2022-12-04 12:34:50 +0100

    remove tuples

--- ts/src/day4.ts
+++ ts/src/day4.ts
@@ -1,27 +1,39 @@
 import fs from 'fs'
 import _ from 'lodash'
 
+type Group = {
+  from: number
+  to: number
+}
+
 const txt = fs.readFileSync('./input', { encoding: 'utf8' })
 
 const rows = txt.split('\n')
 
-const val = rows.map( r => checkOverlap(r, overlap)).reduce( (acc, overlap) => acc + (overlap ? 1 : 0), 0)
+const val = rows
+  .map( r => checkOverlap(r, overlap))
+  .reduce( (acc, overlap) => acc + (overlap ? 1 : 0), 0)
 console.log(val)
 
-const val2 = rows.map(r => checkOverlap(r, overlapAll)).reduce( (acc, overlap) => acc + (overlap ? 1 : 0), 0)
+const val2 = rows
+  .map(r => checkOverlap(r, overlapAll))
+  .reduce( (acc, overlap) => acc + (overlap ? 1 : 0), 0)
 console.log(val2)
 
-function toGroup( g: string ): [number, number] {
+function toGroup( g: string ): Group {
   const [val1, val2] = g.split('-')
 
   if(!val1 || !val2) {
     throw Error()
   }
 
-  return [parseInt(val1, 10), parseInt(val2, 10)]
+  return {
+    from: parseInt(val1, 10),
+    to: parseInt(val2, 10)
+  }
 }
 
-function checkOverlap(str: string, overlapFn: (a: [number, number], b: [number, number]) => boolean): boolean{
+function checkOverlap(str: string, overlapFn: (a: Group, b: Group) => boolean): boolean{
   const [g1str, g2str] = str.split(',')
 
   const g1 = toGroup(g1str)
@@ -30,17 +42,17 @@ function checkOverlap(str: string, overlapFn: (a: [number, number], b: [number, 
   return overlapFn(g1, g2)
 }
 
-function overlap(a: [number, number], b: [number, number]): boolean {
+function overlap(a: Group, b: Group): boolean {
 
-  const aInB = b[1] >= a[1] && b[0] <= a[0]
-  const bInA = a[1] >= b[1] && a[0] <= b[0]
+  const aInB = b.to >= a.to && b.from <= a.from
+  const bInA = a.to >= b.to && a.from <= b.from
 
   return aInB || bInA
 }
 
-function overlapAll(a: [number, number], b: [number, number]): boolean {
-  const avals = _.range(a[0], a[1]+1)
-  const bvals = _.range(b[0], b[1]+1)
+function overlapAll(a: Group, b: Group): boolean {
+  const avals = _.range(a.from, a.to+1)
+  const bvals = _.range(b.from, b.to+1)
   return _.intersection(avals, bvals).length !== 0
 }
 
