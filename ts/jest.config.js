975dffe191caecd4507a4a0d69ba7d93c216e50e
Elia Mazzuoli <zikoel@gmail.com>
2022-12-05 14:41:11 +0100

    configure tests and vscode launches

---
+++ ts/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
+  // globals: {
+  //   'ts-jest': {
+  //     tsconfig: 'server/tsconfig.json'
+  //   }
+  // },
+  // transform: {
+  //   '*.spec.ts': {
+  //     'ts-jest': {
+
+  //     }
+  //   }
+  // }
+}
